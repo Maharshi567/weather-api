a135c023c80731c5f5b7168504d083fdf953f91f
Maharshi567 <maharshidanidhariya001@gmail.com>
2026-09-09 17:58:36 +0530

    New Api Added

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Weather API is running"}
+
+@app.get("/Rajkot")
+def rajkotTemperature():
+    return {"Temperature": "Temperature is 28 degrees"}
